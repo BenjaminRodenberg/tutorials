1cdd7a035f7a2d6c249406b3de1df6fd30ad5dc8
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-06-17 16:11:24 +0200

    Use sigmoid to avoid forces becoming too large.

--- perpendicular-flap/fluid-fake/fake.py
+++ perpendicular-flap/fluid-fake/fake.py
@@ -22,7 +22,7 @@ H = 1.0  # height of the beam
 x_left = 0.0 - 0.5 * W  # x-coordinate of the left side of the beam
 y_bottom = 0.0  # bottom of the beam
 y_top = y_bottom + H  # top of the beam
-F_max = 1.0
+F_max = 1.0 # F_max >= 5 will cause problems with linear structure model
 # define vertices where we want to prove a load
 n = 100  # Number of vertices
 vertices = np.zeros((n, dimensions))
@@ -31,9 +31,12 @@ vertices[:, 1] = np.linspace(y_bottom, y_top, n)  # have n equally disrtibuted v
 vertex_ids = interface.set_mesh_vertices(mesh_name, vertices)
 t = 0
 
+sigmoid = lambda t: 1 / (1 + np.exp(-t))
+force = lambda t: sigmoid(t) * F_max * vertices[:, 1] / H  # linearly increasing load in y direction
+
 if interface.requires_initial_data():
     write_data = np.zeros((n, dimensions))
-    write_data[:, 0] = t * F_max * vertices[:, 1] / H  # linearly increasing load
+    write_data[:, 0] = force(0)
     write_data[:, 1] = 0
     interface.write_data(mesh_name, write_data_name, vertex_ids, write_data)
 
@@ -48,7 +51,7 @@ while interface.is_coupling_ongoing():
     dt = min([solver_dt, precice_dt])
 
     write_data = np.zeros((n, dimensions))
-    write_data[:, 0] = (t + dt) * F_max * vertices[:, 1] / H  # linearly increasing load
+    write_data[:, 0] = force(t + dt)
     write_data[:, 1] = 0
     t += dt
 
